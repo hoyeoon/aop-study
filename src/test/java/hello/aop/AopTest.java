8bfeae5af4f72ac7be0d94db894b828569f9abfc
hoyeoon <hoyeon.backend@gmail.com>
2023-08-10 01:26:21 +0900

    feat: aop 구현

--- src/test/java/hello/aop/AopTest.java
+++ src/test/java/hello/aop/AopTest.java
@@ -2,16 +2,19 @@ package hello.aop;
 
 import hello.aop.order.OrderRepository;
 import hello.aop.order.OrderService;
+import hello.aop.order.aop.AspectV1;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.aop.support.AopUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 @Slf4j
 @SpringBootTest
+@Import(AspectV1.class) // 스프링 bean 으로 등록 된다.
 public class AopTest {
 
     @Autowired
